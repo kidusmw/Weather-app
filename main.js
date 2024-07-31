3594e48241a76f38f20b4721c7d66e5d078a8c1f
Kidus Maru <kiduswubie7110@gmail.com>
2024-07-31 09:10:16 +0300

    Feature added

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
+const searchBtn = document.querySelector(".search button");
+const searchBox = document.querySelector(".search input");
+const weatherIcon = document.querySelector(".weather-icon");
 const apiKey = "ae0a4d1b1c6e89076b37a592e7937940";
 const apiUrl =
 	"https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-const searchBox = document.querySelector(".search input");
-const searchBtn = document.querySelector(".search button");
-
 async function checkWeather(city) {
 	const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-	let data = await response.json();
 
-	console.log(data);
+	if (response.status == 404) {
+		document.querySelector(".error").style.display = "block";
+		document.querySelector(".weather").style.display = "none";
+	} else {
+		let data = await response.json();
 
-	document.querySelector(".city").innerHTML = data.name;
-	document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
-	document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-	document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+		document.querySelector(".city").innerHTML = data.name;
+		document.querySelector(".temp").innerHTML =
+			Math.round(data.main.temp) + "°c";
+		document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+		document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+		if (data.weather[0].main == "Clouds") {
+			weatherIcon.src = "weather-app-img/images/clouds.png";
+		} else if (data.weather[0].main == "Clear") {
+			weatherIcon.src = "weather-app-img/images/clear.png";
+		} else if (data.weather[0].main == "Rain") {
+			weatherIcon.src = "weather-app-img/images/rain.png";
+		} else if (data.weather[0].main == "Mist") {
+			weatherIcon.src = "weather-app-img/images/mist.png";
+		} else if (data.weather[0].main == "Drizzle") {
+			weatherIcon.src = "weather-app-img/images/drizzle.png";
+		}
+
+		document.querySelector(".weather").style.display = "block";
+		document.querySelector(".error").style.display = "none";
+	}
 }
 
 searchBtn.addEventListener("click", () => {
